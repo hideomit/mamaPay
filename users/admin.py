e74e3b2b120a4a2450ecb8c6435331fc0690b8ac
hideo <hideomit@gmail.com>
2020-07-16 21:06:44 +0900

    adminにモデルを反映

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Child
+from .models import Balance
+from .models import Ticket_holding
+from .models import Request
+from .models import History
+from .models import History_ticket
+
+admin.site.register(Child)
+admin.site.register(Balance)
+admin.site.register(Ticket_holding)
+admin.site.register(Request)
+admin.site.register(History)
+admin.site.register(History_ticket)
