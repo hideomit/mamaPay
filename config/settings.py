64eeb541efd46ac40562a8f98aaa12499ab8701c
hideo <hideomit@gmail.com>
2020-09-09 23:18:25 +0900

    'settings修正'

--- config/settings.py
+++ config/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
-#'18.183.255.255'
+ALLOWED_HOSTS = ['18.183.255.255']
+#
 
 # Application definition
 
